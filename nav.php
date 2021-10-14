3d53c253f7a37bbeb352dac5c8d54b8dbd0dd8c2
YuenJiaRong <92509528+YuenJiaRong@users.noreply.github.com>
2021-10-14 16:19:01 +0800

    Add files via upload

---
+++ nav.php
@@ -0,0 +1,7 @@
+<img src="Banner.png" width="960" height="160" alt=""/>
+<nav> 
+    <a href="logcall.php">Log Call</a> 
+    <a href="Update.php">Update</a> 
+    <a href="#">Report</a> 
+    <a href="#">History</a> 
+  </nav>
